7d607ca620ef0353d97548dbf0751d3e6d91fe45
Andrej Suvorau <andrej.suvorau@blitzteam.com>
2019-06-20 22:46:13 +0300

    rotate around the object, new camera controls

--- src/world/editor/private/editor_camera_system.cpp
+++ src/world/editor/private/editor_camera_system.cpp
@@ -1,11 +1,13 @@
 #include "core/ecs/world.h"
 #include "world/editor/editor_camera_component.h"
 #include "world/editor/editor_camera_system.h"
-#include "world/shared/render/camera_single_component.h"
+#include "world/editor/selected_entity_single_component.h"
 #include "world/shared/normal_input_single_component.h"
+#include "world/shared/render/camera_single_component.h"
 #include "world/shared/transform_component.h"
 
 #include <glm/gtc/quaternion.hpp>
+#include <glm/gtx/vector_query.hpp>
 
 namespace hg {
 
@@ -36,6 +38,7 @@ void EditorCameraSystem::update(float elapsed_time) {
 
     auto& camera_single_component = world.ctx<CameraSingleComponent>();
     auto& normal_input_single_component = world.ctx<NormalInputSingleComponent>();
+    auto& selected_entity_single_component = world.ctx<SelectedEntitySingleComponent>();
 
     if (world.valid(camera_single_component.active_camera) && world.has<EditorCameraComponent, TransformComponent>(camera_single_component.active_camera)) {
         auto [editor_camera_component, transform_component] = world.get<EditorCameraComponent, TransformComponent>(camera_single_component.active_camera);
@@ -48,38 +51,73 @@ void EditorCameraSystem::update(float elapsed_time) {
             speed *= CAMERA_SPEED_DECREASE_FACTOR;
         }
 
-        float delta_x = 0.f;
-        if (normal_input_single_component.is_down(Control::KEY_A)) {
-            delta_x = speed;
-        }
-        if (normal_input_single_component.is_down(Control::KEY_D)) {
-            delta_x = -speed;
-        }
+        if (normal_input_single_component.is_down(Control::KEY_LALT) && world.valid(selected_entity_single_component.selected_entity)) {
+            auto& object_transform_component = world.get<TransformComponent>(selected_entity_single_component.selected_entity);
 
-        float delta_y = 0.f;
-        if (normal_input_single_component.is_down(Control::KEY_SPACE)) {
-            delta_y = speed;
-        }
-        if (normal_input_single_component.is_down(Control::KEY_C)) {
-            delta_y = -speed;
-        }
+            const float distance_to_object = glm::distance(transform_component.translation, object_transform_component.translation);
+            if (distance_to_object > glm::epsilon<float>()) {
+                float delta_yaw = 0.f;
+                float delta_pitch = 0.f;
 
-        float delta_z = 0.f;
-        if (normal_input_single_component.is_down(Control::KEY_W)) {
-            delta_z = speed;
-        }
-        if (normal_input_single_component.is_down(Control::KEY_S)) {
-            delta_z = -speed;
-        }
+                if (normal_input_single_component.is_down(Control::BUTTON_RIGHT)) {
+                    const float previous_yaw = editor_camera_component.yaw;
+                    const float previous_pitch = editor_camera_component.pitch;
+                    editor_camera_component.yaw -= normal_input_single_component.get_delta_mouse_x() * MOUSE_SENSITIVITY;
+                    editor_camera_component.pitch += normal_input_single_component.get_delta_mouse_y() * MOUSE_SENSITIVITY;
+                    editor_camera_component.pitch = glm::clamp(editor_camera_component.pitch, -glm::half_pi<float>() * 15.f / 16.f, glm::half_pi<float>() * 15.f / 16.f);
+                    delta_yaw = editor_camera_component.yaw - previous_yaw;
+                    delta_pitch = editor_camera_component.pitch - previous_pitch;
+                }
 
-        if (normal_input_single_component.is_down(Control::BUTTON_RIGHT)) {
-            editor_camera_component.yaw -= normal_input_single_component.get_delta_mouse_x() * MOUSE_SENSITIVITY;
-            editor_camera_component.pitch += normal_input_single_component.get_delta_mouse_y() * MOUSE_SENSITIVITY;
-            editor_camera_component.pitch = glm::clamp(editor_camera_component.pitch, -glm::half_pi<float>(), glm::half_pi<float>());
-        }
+                const glm::vec3 vector1 = transform_component.translation - object_transform_component.translation;
+                const glm::vec3 vector2 = glm::cross(vector1, glm::vec3(0.f, 1.f, 0.f));
 
-        transform_component.rotation = glm::rotate(glm::rotate(glm::quat(1.f, 0.f, 0.f, 0.f), editor_camera_component.yaw, glm::vec3(0.f, 1.f, 0.f)), editor_camera_component.pitch, glm::vec3(1.f, 0.f, 0.f));
-        transform_component.translation += transform_component.rotation * glm::vec3(delta_x, 0.f, 0.f) + glm::vec3(0.f, delta_y, 0.f) + transform_component.rotation * glm::vec3(0.f, 0.f, delta_z);
+                float distance = glm::length(vector1);
+                if (normal_input_single_component.is_down(Control::KEY_W)) {
+                    distance = std::max(distance - speed, 0.1f);
+                }
+                if (normal_input_single_component.is_down(Control::KEY_S)) {
+                    distance = distance + speed;
+                }
+
+                const glm::quat delta_rotation = glm::rotate(glm::rotate(glm::quat(1.f, 0.f, 0.f, 0.f), delta_yaw, glm::vec3(0.f, 1.f, 0.f)), delta_pitch, vector2);
+                transform_component.rotation = glm::rotate(glm::rotate(glm::quat(1.f, 0.f, 0.f, 0.f), editor_camera_component.yaw, glm::vec3(0.f, 1.f, 0.f)), editor_camera_component.pitch, glm::vec3(1.f, 0.f, 0.f));
+                transform_component.translation = object_transform_component.translation + delta_rotation * glm::normalize(vector1) * distance;
+            }
+        } else {
+            float delta_x = 0.f;
+            if (normal_input_single_component.is_down(Control::KEY_A)) {
+                delta_x = speed;
+            }
+            if (normal_input_single_component.is_down(Control::KEY_D)) {
+                delta_x = -speed;
+            }
+
+            float delta_y = 0.f;
+            if (normal_input_single_component.is_down(Control::KEY_E)) {
+                delta_y = speed;
+            }
+            if (normal_input_single_component.is_down(Control::KEY_Q)) {
+                delta_y = -speed;
+            }
+
+            float delta_z = 0.f;
+            if (normal_input_single_component.is_down(Control::KEY_W)) {
+                delta_z = speed;
+            }
+            if (normal_input_single_component.is_down(Control::KEY_S)) {
+                delta_z = -speed;
+            }
+
+            if (normal_input_single_component.is_down(Control::BUTTON_RIGHT)) {
+                editor_camera_component.yaw -= normal_input_single_component.get_delta_mouse_x() * MOUSE_SENSITIVITY;
+                editor_camera_component.pitch += normal_input_single_component.get_delta_mouse_y() * MOUSE_SENSITIVITY;
+                editor_camera_component.pitch = glm::clamp(editor_camera_component.pitch, -glm::half_pi<float>() * 15.f / 16.f, glm::half_pi<float>() * 15.f / 16.f);
+            }
+
+            transform_component.rotation = glm::rotate(glm::rotate(glm::quat(1.f, 0.f, 0.f, 0.f), editor_camera_component.yaw, glm::vec3(0.f, 1.f, 0.f)), editor_camera_component.pitch, glm::vec3(1.f, 0.f, 0.f));
+            transform_component.translation += transform_component.rotation * glm::vec3(delta_x, 0.f, 0.f) + glm::vec3(0.f, delta_y, 0.f) + transform_component.rotation * glm::vec3(0.f, 0.f, delta_z);
+        }
     }
 }
 
